05795f7a7089fda388cc9ebbf74ce2b6d7ce5338
Ujjwal Karn <43888360+UJKEM@users.noreply.github.com>
2023-08-24 00:31:47 +0530

    Third commit

--- controllers/gitController.js
+++ controllers/gitController.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
+const ansiRegex = require('ansi-regex');
+
 
 const gitIdController = (async (req, res, next) => {
-    console.log("In git id controller")
    const {owner, repository, oid} = req.params;
 
     if(!owner || !repository || !oid){
@@ -22,13 +23,12 @@ try{
 
  const {data} = await axios.get(`https://api.github.com/repos/${owner}/${repository}/commits/${oid}`,{
     headers:{
-        'Authorization':"Bearer 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",
+        'Authorization':"Bearer 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",
         'Accept': "application/vnd.github+json",
         'X-GitHub-Api-Version': "2022-11-28"
     },
 })
 
-// console.log(data);
 return res.status(200).json({data});
 }
 
@@ -63,13 +63,19 @@ const gitCommitController = (async (req, res, next) => {
 
         const {data} = await axios.get(`https://api.github.com/repos/${owner}/${repository}/compare/${base}...${head}`,{
            headers:{
-               'Authorization':"Bearer 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",
+               'Authorization':"Bearer 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",
                'Accept': "application/vnd.github+json",
                'X-GitHub-Api-Version': "2022-11-28"
            },
        })
-       
-       return res.status(200).json({data});
+    //    const {data:diff} = await axios.get(data.diff_url);
+    //    console.log(diff);
+    const { data: diffData } = await axios.get(data.diff_url);
+
+// Remove ANSI escape codes from the diff content
+const diffWithoutAnsi = diffData.replace(ansiRegex(), '');
+
+return res.status(200).send(diffWithoutAnsi);
        }
        
        catch(error){
